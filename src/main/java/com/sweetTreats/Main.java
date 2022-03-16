f7c919d20d85dbd17174bcc42c43b5fdba232434
Amanuel <amanuel.tedros@capgemini.com>
2022-03-16 16:30:00 +0000

    Initial commit

---
+++ src/main/java/com/sweetTreats/Main.java
@@ -0,0 +1,14 @@
+package com.sweetTreats;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
