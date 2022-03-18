b810fb0df9aba1c7460b028bbbaf85d3bf1804d4
Amanuel <amanuel.tedros@capgemini.com>
2022-03-18 15:50:55 +0000

    Input value changed to String and time in working fine

--- src/main/java/com/sweetTreats/Order.java
+++ src/main/java/com/sweetTreats/Order.java
@@ -1,15 +1,16 @@
 package com.sweetTreats;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Order {
     private LocalTime orderHour;
     private double customerDistance;
     private boolean IsRefrigerated;
 
-    public Order(int orderHour, double customerDistance, boolean isRefrigerated) {
-        LocalTime time = LocalTime.of(orderHour, 0);
-        this.orderHour = time;
+    public Order(String orderHour, double customerDistance, boolean isRefrigerated) {
+//        LocalTime time = LocalTime.of(orderHour, 0);
+        this.orderHour = LocalTime.parse(orderHour, DateTimeFormatter.ISO_LOCAL_TIME);
         this.customerDistance = customerDistance;
         IsRefrigerated = isRefrigerated;
     }
