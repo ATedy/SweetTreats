9908332d9f7dd1275fcd8c665bc52a4699ead0d3
Amanuel <amanuel.tedros@capgemini.com>
2022-03-18 16:23:20 +0000

    Console and file logger added

--- src/main/java/com/sweetTreats/SweetTreatOrderTaker.java
+++ src/main/java/com/sweetTreats/SweetTreatOrderTaker.java
@@ -2,7 +2,7 @@ package com.sweetTreats;
 
 public class SweetTreatOrderTaker {
     public static void main(String[] args) {
-        SweetTreats.courierSelector(new Order("19:00", 1.5, true));
+        SweetTreats.courierSelector(new Order("14:00", 1.5, true));
 
 
     }
--- src/main/java/com/sweetTreats/SweetTreats.java
+++ src/main/java/com/sweetTreats/SweetTreats.java
@@ -1,14 +1,30 @@
 package com.sweetTreats;
 
+import java.io.IOException;
 import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.logging.Logger;
+import java.util.logging.*;
 import java.util.stream.Collectors;
 
 public class SweetTreats {
-    private static final Logger LOGGER = Logger.getLogger(SweetTreats.class.getName());
+    private static final Logger CONSOLE = Logger.getLogger(SweetTreats.class.getName());
+
+    static {
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.INFO);
+        FileHandler fileHandler = null;
+        try {
+            fileHandler = new FileHandler("sweatTreatLogger.xml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        XMLFormatter formatter = new XMLFormatter();
+        fileHandler.setFormatter(formatter);
+        logger.addHandler(fileHandler);
+
+    }
 
     static List<Courier> couriers =  Arrays.asList(
             new Courier("Bobby", 5, true, 1.75, "09:00" , "13:00" ),
@@ -30,12 +46,12 @@ public class SweetTreats {
           Comparator<Courier> comparator = Comparator.comparing( courier -> courier.getChargePerMile() );
           Courier cheapestCourier = availableCourier.stream().min(comparator).get();
 
-          LOGGER.info("Cheapest available courier details" + "\n" + "Courier-Name: " + cheapestCourier.getName() + "\n" + "Max-Miles:" + cheapestCourier.getMaxDeliveryMilesMiles() +
+          CONSOLE.info("Cheapest available courier details" + "\n" + "Courier-Name: " + cheapestCourier.getName() + "\n" + "Max-Miles:" + cheapestCourier.getMaxDeliveryMilesMiles() +
                   "\n" + "Charge Per Mile: " + cheapestCourier.getChargePerMile());
           return  cheapestCourier;
 
       }else{
-          LOGGER.info("Sorry, it seems there's no available courier at the Moment");
+          CONSOLE.info("Sorry, it seems there's no available courier at the Moment");
           return null;
       }
 
