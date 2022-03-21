5797ae9d5736ec79536ec46f9a2cc034ca0a4e2d
Amanuel <amanuel.tedros@capgemini.com>
2022-03-21 09:40:35 +0000

    logging feature branched merged and code refactored

--- src/main/java/com/sweetTreats/SweetTreatOrderTaker.java
+++ src/main/java/com/sweetTreats/SweetTreatOrderTaker.java
@@ -3,6 +3,8 @@ package com.sweetTreats;
 public class SweetTreatOrderTaker {
     public static void main(String[] args) {
         SweetTreats.courierSelector(new Order("14:00", 1.5, true));
+//        SweetTreats.courierSelector(new Order("19:00", 1.5, true));
+//        SweetTreats.courierSelector(new Order("11:00", 1.5, false));
 
 
     }
